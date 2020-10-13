032d31575aa5993cf3cd13162b0ed299e3e9112f
mshahrasbi <mshahrasbi@msn.com>
2020-10-13 15:10:03 -0700

    Implemented form configuration fot nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,12 +9,14 @@ class PortfoliosController < ApplicationController
 
     def new
         @portfolio_item = Portfolio.new
+
+        3.times { @portfolio_item.technologies.build }
     end
     
     def create
         # here we are telling the form what to access, so we are saying portfolio we are going
         # to permit a title, subtitle, and body
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
         respond_to do |format|
           if @portfolio_item.save 
